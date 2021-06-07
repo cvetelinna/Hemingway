bce3fe11debeb204e434596f4fc1556a8af9a4b6
cvetelinna <tsvetelina.peytcheva2@gmail.com>
2021-06-07 15:38:54 +0300

    Calculate income and expenses

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,16 +8,16 @@ public class Main {
     public static void main(String[] args) {
     List<Employee> ems = new ArrayList<>();
 
-        ems.add(new Employee(EmployeeType.MANAGER));
-        ems.add(new Employee(EmployeeType.OPERATOR));
-        ems.add(new Employee(EmployeeType.OPERATOR));
-        ems.add(new Employee(EmployeeType.OPERATOR));
-        ems.add(new Employee(EmployeeType.OPERATOR));
-        ems.add(new Employee(EmployeeType.OPERATOR));
-        ems.add(new Employee(EmployeeType.MANAGER));
+        ems.add(new Employee(EmployeeType.MANAGER, 800));
+        ems.add(new Employee(EmployeeType.OPERATOR, 800));
+        ems.add(new Employee(EmployeeType.OPERATOR,  800));
+        ems.add(new Employee(EmployeeType.OPERATOR, 800));
+        ems.add(new Employee(EmployeeType.OPERATOR, 800));
+        ems.add(new Employee(EmployeeType.OPERATOR, 800));
+        ems.add(new Employee(EmployeeType.MANAGER,800));
 
-        Edition editionTest = new Edition("Qnko", 300, PageFormat.А3, PaperType.GLOSSY, 10);
-        Edition test = new Edition("Harry Potter", 300, PageFormat.А3 , PaperType.GLOSSY, 30);
+        Edition editionTest = new Edition("Qnko", 3000, PageFormat.А3, PaperType.GLOSSY, 100);
+        Edition test = new Edition("Harry Potter", 3000, PageFormat.А3 , PaperType.GLOSSY, 300);
         List<Edition> editions = new ArrayList<>();
 
         editions.add(test);
@@ -29,10 +29,28 @@ public class Main {
             sum += edt.price() * edt.getRequrestedCount();
             win += edt.calculateBookIncome();
         }
+
+        double bonus = 0;
+        double managerBonus=0;
+
+        for(var employee : ems){
+            double brute = employee.getSalary();
+            bonus += brute;
+        }
+        double tmp = win - (sum + bonus);
+        if ( tmp > 0) {
+            managerBonus += tmp;
+            var printManagerBonus = String.format("Managers should separate %s between them", managerBonus);
+            System.out.println(printManagerBonus);
+        }
+
         var printBookExpenses = String.format("Material expenses are %s leva", sum);
         var printBookIncome = String.format("We win %s leva from books", win);
+        var printSalaryExpenses = String.format("Salary expenses are %s", bonus );
+
         System.out.println(printBookExpenses);
         System.out.println(printBookIncome);
+        System.out.println(printSalaryExpenses);
 
         List<Machine> mcs = new ArrayList<>();
         mcs.add(new Machine(Color.BLACKANDWHITE, 3000, 2000));
@@ -40,10 +58,9 @@ public class Main {
         mcs.add(new Machine(Color.BLACKANDWHITE, 3000, 2000));
 
 
-    PrintingHouse ph = new PrintingHouse(12000, 12200, 60, ems, mcs);
-        System.out.println(ph.sumSalaryExpenses());
-        ph.startPrinting(test);
-        ph.startPrinting(editionTest);
+    PrintingHouse ph = new PrintingHouse(ems, mcs);
+        //ph.startPrinting(test);
+        //ph.startPrinting(editionTest);
 
     }
 
--- src/com/company/PrintingHouse.java
+++ src/com/company/PrintingHouse.java
@@ -3,31 +3,12 @@ import java.util.List;
 
 public class PrintingHouse {
 
-    private double expectedIncome;
-    private double actualIncome;
-    private int printedEditionsCount;
     private List<Employee> employees;
-    private static double salary = 800;
     private List<Machine> machines;
 
-    public PrintingHouse(double expectedIncome, double actualIncome, int printedEditionsCount, List<Employee> employees, List<Machine> machines) {
-        this.expectedIncome = expectedIncome;
-        this.actualIncome = actualIncome;
-        this.printedEditionsCount = printedEditionsCount;
+    public PrintingHouse( List<Employee> employees, List<Machine> machines) {
         this.employees = employees;
         this.machines = machines;
-    }
-
-    public double getExpectedIncome() {
-        return expectedIncome;
-    }
-
-    public double getActualIncome() {
-        return actualIncome;
-    }
-
-    public int getPrintedEditionsCount() {
-        return printedEditionsCount;
     }
 
     public List<Employee> getEmployees() {
@@ -41,31 +22,9 @@ public class PrintingHouse {
     @Override
     public String toString() {
         return "PrintingHouse{" +
-                ", expectedIncome=" + expectedIncome +
-                ", actualIncome=" + actualIncome +
-                ", printedEditionsCount=" + printedEditionsCount +
                 ", employees=" + employees +
                 ", machines=" + machines +
                 '}';
-    }
-
-    public double sumSalaryExpenses() {
-        double managerTmp = 0;
-        int managerCounter = 0;
-        for (Employee employee : employees)
-        {
-            if (employee.isManager())
-            {
-                managerCounter++;
-                if (actualIncome > expectedIncome ) {
-                    double diff = actualIncome - expectedIncome;
-                    managerTmp += (int)((double)( salary + (diff / managerCounter)));
-                    int operatorsCounter = employees.size() - managerCounter;
-                    return (salary * operatorsCounter) + (managerTmp * managerCounter);
-                }else return salary * employees.size();
-            }
-        }
-        return salary * employees.size();
     }
 
     public void startPrinting(Edition ed){
